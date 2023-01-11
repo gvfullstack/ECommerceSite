8fd30284d94a8fe29bbf3cb723f17b1c01970b5e
Gabriel <lsvillegas7@yahoo.com>
2023-01-11 11:51:32 -0800

    removed unnecessary validation files

--- src/passwordSignInValidation.js
+++
@@ -1,4 +0,0 @@
-const passwordSignInValidation = (password, passwordAgain) => password === passwordAgain ? "" : "Incorrect password. Please try again."
-
-
-export default passwordSignInValidation
--- src/passwordValidation.js
+++
@@ -1,9 +0,0 @@
-const passwordCheck = password => {
-    let errorMessages = ['Password should have between 6-13 characters.', 'Password should have one upper case letter.', 
-    'Password should have one lower case letter.', "Password should have one number.", "Password should have no spaces." ]
-
-    return(
-    password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,13}$/) ? '' : errorMessages
-    )
-}
-export default passwordCheck
